34e36432c061e80c2e1f3a83c31c692f1569010f
BoogieNaipe <marcelovldgomes@outlook.com>
2024-10-08 18:15:51 -0300

    1.000001

--- src/main/java/org/BoogieNaipe/Jogo.java
+++ src/main/java/org/BoogieNaipe/Jogo.java
@@ -6,6 +6,9 @@ import java.awt.*;
 public class Jogo {
 
     JFrame janela;
+    Container container;
+    JPanel tituloPanel;
+    JLabel tituloLabel;
 
     public static void main(String[] args) {
 
@@ -20,5 +23,14 @@ public class Jogo {
         janela.getContentPane().setBackground(Color.black);
         janela.setLayout(null);
         janela.setVisible(true);
+        janela.setResizable(false);
+        container = janela.getContentPane();
+
+        tituloPanel = new JPanel();
+        tituloPanel.setBounds(100,100,750,150);
+        tituloPanel.setBackground(Color.black);
+        tituloLabel = new JLabel("Aventura!");
+        tituloLabel.setForeground(Color.white);
+        container.add(tituloPanel);
     }
 }
