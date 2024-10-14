66ba164abd775fbf91b5695791dc748c290f3848
BoogieNaipe <marcelovldgomes@outlook.com>
2024-10-14 18:24:56 -0300

    Tela inicial Concluida

--- src/main/java/org/BoogieNaipe/Jogo.java
+++ src/main/java/org/BoogieNaipe/Jogo.java
@@ -1,5 +1,7 @@
 package org.BoogieNaipe;
 
+import org.BoogieNaipe.ASCII.TituloInicial;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,30 +9,53 @@ public class Jogo {
 
     JFrame janela;
     Container container;
-    JPanel tituloPanel;
-    JLabel tituloLabel;
+    JPanel tituloInicialPanel, startBotaoPanel;
+    JButton startBotao;
+    Font fontNormal = new Font ("TimesRoman", Font.PLAIN, 20);
+
 
     public static void main(String[] args) {
-
         new Jogo();
-
     }
 
-    public Jogo(){
+    public Jogo() {
         janela = new JFrame();
-        janela.setSize(960,720);
+        janela.setSize(800, 600);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.getContentPane().setBackground(Color.black);
         janela.setLayout(null);
-        janela.setVisible(true);
         janela.setResizable(false);
         container = janela.getContentPane();
 
-        tituloPanel = new JPanel();
-        tituloPanel.setBounds(100,100,750,150);
-        tituloPanel.setBackground(Color.black);
-        tituloLabel = new JLabel("Aventura!");
+        tituloInicialPanel = new JPanel();
+        tituloInicialPanel.setBounds(100, 100, 600, 150);
+        tituloInicialPanel.setBackground(Color.black);
+
+        JLabel tituloLabel = new JLabel(TituloInicial.getTexto());
         tituloLabel.setForeground(Color.white);
-        container.add(tituloPanel);
+
+        startBotaoPanel = new JPanel();
+        startBotaoPanel.setBounds(300,400,200,100);
+        startBotaoPanel.setBackground(Color.black);
+
+        startBotao = new JButton("INICIAR!");
+        startBotao.setBackground(Color.black);
+        startBotao.setForeground(Color.white);
+        startBotao.setFont(fontNormal);
+        ;
+
+
+        tituloInicialPanel.add(tituloLabel);
+        container.add(tituloInicialPanel);
+        container.add(startBotaoPanel);
+        startBotaoPanel.add(startBotao);
+
+
+
+
+
+
+        janela.setVisible(true);
+
     }
 }
